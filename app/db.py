35b3ace3e0720d35b2af1f95c8cf76ba1ae82945
Yuqing Wu <ywu20@stuy.edu>
2021-10-29 12:33:09 -0400

    fixed create table

--- app/db.py
+++ app/db.py
@@ -14,18 +14,25 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
 #create tables for database
-command = "CREATE TABLE"
+command = "CREATE TABLE "
 
 # Creating table with username | password | stories contributed table
 c.execute (command + "user_info (username TEXT, password TEXT, stories_contributed TEXT)")
 
 # creating table with story title | user contributed | story entry
-c.execute (command + "story (title TEXT, user TEXT, entry TEXT)")
+c.execute (command + "story (title TEXT, contributor TEXT, entry TEXT)")
 
+#testing
+c.execute ("INSERT INTO story VALUES('story1', 'user1', 'hello')")
 
-def write_to_story(title, user_contributed, entry):
+#doesn't work yet
+def write_to_story(_title, _contributor, _entry):
     contributor = ""
     entry = ""
-    c.execute ("INSERT INTO story VALUES(?, ?,?)", title, contributor, entry)
+    #c.execute ("INSERT INTO story VALUES(?, ?,?)", _title, _contributor, _entry)
 
-    #c.execute("SELECT ")
+    c.execute("SELECT contributor FROM story WHERE title = _title")
+    contributor_list = c.fetchall()
+    print(contributor_list)
+
+write_to_story("story1", "user2", "world")
