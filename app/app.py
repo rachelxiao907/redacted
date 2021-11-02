e9a7f7061998d8891819c0d8338ab4e6bc2bc74a
Yuqing Wu <ywu20@stuy.edu>
2021-11-02 14:04:34 -0400

    add story pages

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask import request           #facilitate form submission
 from flask import session           #allow for session creation/maintenance
 from flask import redirect
 from os import urandom
-from db import get_login, add_login
+import db
 import sqlite3   #enable control of an sqlite database
 
 app = Flask(__name__)    #create Flask object
@@ -22,7 +22,7 @@ def disp_loginpage():
     # print(request.args['username']) -- does NOT work - this has not been defined yet - causes error
     print("***DIAG: request.headers ***")
     print(request.headers)
-    
+
     # checks for request method and gets the input
     data = []
     if(request.method == "GET"):
@@ -48,7 +48,7 @@ def disp_loginpage():
         error = "" # the error message
         # a try catch block in case anything unexpected happens
         try:
-            error = get_login(name_input, pass_input)
+            error = db.get_login(name_input, pass_input)
             if(error == ""):
                 session["login"] = name_input
                 print("hello")
@@ -91,7 +91,7 @@ def create_account_render():
             error = "Password fields must match!"
         else:
             try:
-                add_login(name_input, pass_input) # try to add u/p pair to db
+                db.add_login(name_input, pass_input) # try to add u/p pair to db
                 return redirect("/") # go back to main login page
             except sqlite3.IntegrityError: # will throw this error if the username is a duplicate
                 error = "Username already exists!"
@@ -100,9 +100,48 @@ def create_account_render():
     # render the page
     return render_template('create_account.html')
 
+"""
+to see how the add story functions work:
+    uncomment  -- testing for add story -- part in db file, should just have the three manually add story
+    c.execute statement
+
+    create account & login
+
+    type in the search bar /add instead of /home, you should see some number of story2 or story3 (because
+    story1 is marked as contributed manually in the get_addable_story function for testing)
+
+    click on any of them, you should see last entries at the top and type in the input text section, click submitting
+
+    you should be redirected to the add page once you click submit.
+
+    click on the same story again, the last entry should show up as what you just entered
+"""
+
+#stories got added is weird because of the number of times the scripts in db file is called,
+# should be fixed when everything is linked together and when i don't test with manual insert intos.
+@app.route("/add",methods=['GET', 'POST'])
+def add_story_list():
+
+    #when you login the site, session["login"] would store which use is using
+    story_list = db.get_story_addable(session["login"])
+    if(request.method == 'POST'):
+        db.add_to_story(request.form["title"], session["login"],request.form["entry"])
+
+        #terminal testing prints
+        #print("added to story")
+        #print(get_story_last_entry(request.form["title"]))
+    return render_template("add.html", collection = story_list)
+
+#after submitting would go to the add page
+@app.route("/add/<story>")
+def add_a_story(story):
+    #displays the contributor and the last entry of story
+    last_entry = db.get_story_last_entry(story)
+    return render_template("add_story.html",last_contributor=last_entry[0],last_entry = last_entry[1], title = story)
 
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
     app.run()
+    db.db.close()
