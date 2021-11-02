33c6efd5d1708ce6a12f35a191953778473ed834
Yuqing Wu <ywu20@stuy.edu>
2021-11-02 18:08:17 -0400

    uncomment close database

--- app/app.py
+++ app/app.py
@@ -151,4 +151,4 @@ if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True
     app.run()
-    #db.db.close()
+    db.db.close()
