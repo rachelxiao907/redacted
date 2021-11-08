98d78084e3e1848f6be273eb0d3ade06873d9ab2
Michael Borczuk <michaelborczuk@gmail.com>
2021-11-08 09:18:16 -0500

    fix bug with adding stories

--- app/app.py
+++ app/app.py
@@ -150,18 +150,13 @@ def add_story_list():
 @app.route("/add/<story>")
 def add_a_story(story):
     if("login" in session and not(session["login"] == False)):
-        #displays the contributor and the last entry of story
-        last_entry = db.get_story_last_entry(story)
-        title = ""
-        for i in story:
-            if i == " " or i == "\\":
-                title+="_"
-            else:
-                title += i
-        print(last_entry)
-        return render_template("add_story.html", last_contributor=last_entry[0],last_entry = last_entry[1], title = title)
-    else:
-        return redirect("/")
+        story_list = db.get_story_addable(session["login"])
+        if(story in story_list):
+            #displays the contributor and the last entry of story
+            last_entry = db.get_story_last_entry(story)
+            print(last_entry)
+            return render_template("add_story.html", last_contributor=last_entry[0],last_entry = last_entry[1], title = story)
+    return redirect("/")
 
 
 @app.route("/create", methods=['GET', 'POST'])
